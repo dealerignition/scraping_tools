4584abad5b8ffa1f553215abfcb15dc36f238981
Ethan McGee <bulletshot60@gmail.com>
2012-10-17 15:56:59 -0400

    update cambria/karastan

--- cambria/process_json.rb
+++ cambria/process_json.rb
@@ -3,7 +3,7 @@ require 'nokogiri'
 require 'open-uri'
 require 'json/pure'
 
-countertops = open('http://www.cambriausa.com/default/includes/themes/default/javascripts/data_colors.js').read.gsub(/(Desert[^"]*Collection &trade;)/, "Desert Collection").gsub("&trade;", "")
+countertops = open('http://www.cambriausa.com/_cassette/asset/public/javascripts/color-tiles.js').read.gsub(" &trade;", "")
 countertops = countertops.scan(/\(function\(\$, global\)\{global\.CAMBRIA = global\.CAMBRIA \|\| \{\}; CAMBRIA\.tiles = (.*)\}\(jQuery, this\)\);/)[0][0]
 countertops = JSON.parse(countertops)
 
@@ -15,15 +15,16 @@ new_countertop_color = {:color_name => "", :color_type => "", :color_image_url =
 countertops.each_key do |k|
   countertop = new_countertop.clone
   countertop[:name] = countertops[k]["name"]
-  countertop[:warranty] = "At Cambria, we offer a Lifetime Limited Warranty against manufacturing defects. Feel the confidence and freedom of living with your Cambria, knowing we stand firmly behind every product we make and every customer we earn. For more information, <a href='http://www.cambriausa.com/linkservid/456DD93C-6643-45F8-B79BC2A1669C13A1/showMeta/0/'>download the Lifetime Limited Warranty</a>. To register your Cambria for our Lifetime Limited Warranty, simply complete the online <a href='http://www.cambriausa.com/living-with-cambria/warranty-registration/warranty-registration-form/'>Warranty Registration Form</a>."
+  countertop[:warranty] = "At Cambria, we offer a Lifetime Limited Warranty against manufacturing defects. Feel the confidence and freedom of living with your Cambria, knowing we stand firmly behind every product we make and every customer we earn. For more information, download the Lifetime Limited Warranty. To register your Cambria for our Lifetime Limited Warranty, simply complete the online Warranty Registration Form."
   countertop[:collection_name] = countertops[k]["collection"]
-  countertop[:details] = Nokogiri::HTML(open("http://www.cambriausa.com"+countertops[k]["detailURL"])).xpath("//div[@class='main-primary color-detail detail-#{countertops[k]["name"].downcase.gsub(" ", "-")}']/p")[0].inner_text
+  countertop[:details] = Nokogiri::HTML(open("http://www.cambriausa.com"+countertops[k]["detailURL"])).xpath("//div[@class='main-primary color-detail']/p")[0].inner_text
+  countertop[:details].strip!
   
   countertop[:styles] = []
   countertop[:colors] = []
 
   countertops[k]["qualities"].each do |quality|
-    if quality.scan('Colors') != []
+    if quality.scan('Color') != []
       countertop[:colors] << {
         :color_name => countertops[k]["name"],
         :color_type => quality.gsub("_"," "),
@@ -42,7 +43,7 @@ end
 
 i = 1
 countertop_arr.each do |countertop|
-  puts "#{i}\t#{countertop[:name]}\t#{countertop[:details]}\t#{countertop[:warranty]}\t#{countertop[:collection_name]}\t#{countertop[:room_image_url]}\t5"
+  puts "#{i}\t#{countertop[:name]}\t#{countertop[:details]}\t#{countertop[:warranty]}\t#{countertop[:collection_name]}\t#{countertop[:room_image_url]}"
   i = i + 1
 end
 
